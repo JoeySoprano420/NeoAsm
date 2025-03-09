60bfd123797a35f197f105a4a2e3205f0bde42a8
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-03-08 21:33:56 -0500

    Create AST.py

---
+++ AST.py
@@ -0,0 +1,43 @@
+[
+    # Map Declaration Node
+    {
+        "type": "map", 
+        "identifier": "A", 
+        "entries": [
+            {"key": "src", "value": "RAM"}, 
+            {"key": "dst", "value": "GPU"}
+        ]
+    },
+
+    # Variable Declaration Node
+    {
+        "type": "variable", 
+        "var_type": "INT", 
+        "identifier": "temp", 
+        "attributes": {
+            "range": "0..255", 
+            "check": "rigid"
+        }
+    },
+
+    # AOT Declaration Node
+    {
+        "type": "AOT", 
+        "identifier": "test", 
+        "attributes": {
+            "type": "STATIC", 
+            "size": "1024"
+        }
+    },
+
+    # Packetized Execution Declaration Node
+    {
+        "type": "packet", 
+        "identifier": "data", 
+        "attributes": {
+            "size": "512", 
+            "exec": "EXECUTE", 
+            "priority": "RAM"
+        }
+    }
+]
