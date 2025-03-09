bc9176fd8a720944beaf50800b037e9edf3bb7c3
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-03-08 21:46:48 -0500

    Create Linux (Debian-based systems.sh

---
+++ Linux (Debian-based systems.sh
@@ -0,0 +1,4 @@
+# Automatically select a faster mirror (for Ubuntu/Debian)
+sudo apt-get install netselect-apt
+sudo netselect-apt
+# This automatically selects the fastest mirror and updates the apt configuration
